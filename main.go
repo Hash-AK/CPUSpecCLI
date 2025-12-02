45578443c4138a3e9532822fb740e4a465c6216b
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-12-02 12:17:27 -0500

    finishing parsing the json

--- main.go
+++ main.go
@@ -13,12 +13,30 @@ type CPUs struct {
 	CPUs []CPU `json:"cpus"`
 }
 type CPU struct {
-	ID                 string `json:"id"`
-	Name               string `json:"name"`
-	Brand              string `json:"brand"`
-	Generation         int    `json:"generation"`
-	GenerationCodename string `json:"generation_codename"`
-	Series             string `json:"series"`
+	ID                 string   `json:"id"`
+	Name               string   `json:"name"`
+	Brand              string   `json:"brand"`
+	Generation         int      `json:"generation"`
+	GenerationCodename string   `json:"generation_codename"`
+	Series             string   `json:"series"`
+	Specs              Specs    `json:"specs"`
+	Features           []string `json:"features"`
+	Overclockable      bool     `json:"overclockable"`
+	ReleaseDate        string   `json:"release_date"`
+}
+type Specs struct {
+	Cores                    int     `json:"cores"`
+	Threads                  int     `json:"threads"`
+	CacheL1KB                float64 `json:"cache_l1_kb"`
+	CacheL2MB                float64 `json:"cache_l2_mb"`
+	CacheL3MB                float64 `json:"cache_l3_mb"`
+	BaseFrequencyGHz         float64 `json:"base_freq_ghz"`
+	BoostFrequencyGHz        float64 `json:"boost_freq_ghz"`
+	TDPWatts                 int     `json:"tdp_watts"`
+	Socket                   string  `json:"socket"`
+	Architecture             string  `json:"architecture"`
+	IntegratedGPU            string  `json:"integrated_gpu"`
+	MaximumSupportedMemoryGB int     `json:"max_mem_supported_gb"`
 }
 
 func main() {
@@ -35,6 +53,7 @@ func main() {
 		fmt.Println(cpus.CPUs[i].Generation)
 		fmt.Println(cpus.CPUs[i].GenerationCodename)
 		fmt.Println(cpus.CPUs[i].Series)
+		fmt.Println(cpus.CPUs[i].Specs.Cores)
 
 	}
 
